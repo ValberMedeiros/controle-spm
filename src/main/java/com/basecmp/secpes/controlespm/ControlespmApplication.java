a2d8057e615870cf5105661d65eeb24fa10330dc
valber Junior <dev.juniormedeiros@gmail.com>
2020-10-16 11:17:37 -0300

    Initial commit

---
+++ src/main/java/com/basecmp/secpes/controlespm/ControlespmApplication.java
@@ -0,0 +1,13 @@
+package com.basecmp.secpes.controlespm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlespmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlespmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/basecmp/secpes/controlespm/ControlespmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basecmp.secpes.controlespm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlespmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
